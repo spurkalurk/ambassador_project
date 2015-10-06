2ed4700430763383777604c5b26033850075121c
Amanda <a.vacharat@gmail.com>
2015-10-05 20:53:37 -0400

    form, adds clicks, edit, delete. problems with route param

---
+++ controllers.js
@@ -0,0 +1,67 @@
+
+
+var app = angular.module('formApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/',
+		{
+			templateUrl: '/form.html',
+			controller: 'FormController'
+		})
+
+		.when("/:name",
+   		 {
+	      templateUrl: "/app.html",
+	      controller: "FormController",
+	      
+   		 })
+
+		// .otherwise(
+		// {
+		// 	redirectTo: '/404',
+		// 	templateUrl: '/404.html'
+		// });
+});
+
+
+
+app.controller('HeaderController', ['$location', function($location) {
+    var self = this;
+
+    self.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+}]);
+
+app.controller('FormController', [function($routeParams, $location) {
+
+	var self = this;
+
+	self.referrals= [
+		{ name: 'Bob', clicks: 0},
+		{ name: 'Harry', clicks: 0}
+	];
+
+	self.add = function(entry) {
+		var newRef = {
+			name: entry,
+			clicks: 0
+		};
+		self.referrals.push(newRef);
+
+		console.log(self.name);
+
+		$location.search('name', self.name);
+
+	};
+
+
+	self.remove = function(item) {
+		self.referrals.splice(self.referrals.indexOf(item), 1);
+	};
+
+
+
+
+}]);
